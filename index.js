fc24f1ba49ad01f21c87be0c3b2d7b90d2ee5fa1
metaraine <raineorshine@gmail.com>
2014-12-10 01:41:22 -0700

    basic chaining

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 'use strict'
 
-module.exports = {
+function id (x) { return x }
+function second (x,y) { return y }
+
+function shackles(host) {
+
+	// a mutating value which the host function wrappers will have closure over
+	var value
+
+	// the chaining object that houses all the wrapped functions
+	var container = {
+		value: function() {
+			return value
+		},
+		toString: function() {
+			return value.toString()
+		}
+	}
+
+	// a wrapper function that invokes the host function with the mutator value as the first argument
+	// attaches to the container for chaining
+	// must be part of a separate create method so that f is retained (it gets lost in the for loop iteration otherwise)
+	function createMutator(f) {
+		return function() {
+			var args = Array.prototype.concat.apply([value], [Array.prototype.slice.call(arguments)])
+			value = f.apply(host, args)
+			// console.log('arguments', arguments, 'args', args, 'value', value)
+			return container
+		}
+	}
+
+	// move each property from the host onto the container
+	for(var key in host) {
+
+		// protect the value property
+		if(key === 'value') {
+			console.warn('value is reserved for getting the result of the chain. The host object has value property which will be ignored.')
+			continue
+		}
+
+		// if the property value is not a function, wrap it in the identity function
+		var f = typeof host[key] === 'function' ?
+			host[key] :
+			id.bind(null, host[key])
+
+		container[key] = createMutator(f)
+	}
+
+	// return a function that can start the chain while setting an initial value
+	return createMutator(second)
 }
+
+module.exports = shackles
--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,67 @@
 'use strict'
+
 var assert = require('insist')
 var shackles = require('../index.js')
+var _ = require('lodash')
 
-// it('should do something', function () {
-// 	assert.strictEqual(true, false)
-// })
+describe('shackles', function() {
+
+	it('should be able to set an initial value and retrieve it with .value()', function () {
+		var C = shackles({})
+		var result = C('test').value()
+		assert.equal(result, 'test')
+	})
+
+	it('should have a toString method that returns the string represention of the boxed value', function () {
+		var C = shackles({})
+		var result = C('test').toString()
+		assert.equal(result, 'test')
+	})
+
+	it('should chain a string library', function () {
+		var stringlib = {
+			prepend: function(str, chr, chr2) {
+				return (chr || '') + (chr2 || '') + str
+			},
+			append: function(str, chr, chr2) {
+				return str + (chr || '') + (chr2 || '')
+			}
+		}
+
+		var C = shackles(stringlib)
+
+		var result = C('Hello')
+			.prepend('(')
+			.append('!', '?')
+			.append(')')
+			.value()
+
+		assert.equal(result, '(Hello!?)')
+	})
+
+	it('should chain lodash', function () {
+
+		var C = shackles(_)
+
+		var result = C([1,2,3])
+			.map(function (x) { return x*x })
+			.filter(function (x) { return x > 2 })
+			.value()
+
+		assert.deepEqual(result, [4, 9])
+	})
+
+	it('should handle scalar properties', function () {
+
+		var C = shackles({
+			num: 10
+		})
+
+		var result = C('dummy')
+			.num()
+			.value()
+
+		assert.deepEqual(result, 10)
+	})
+
+})
